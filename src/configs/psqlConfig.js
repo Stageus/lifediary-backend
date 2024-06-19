2eaad07280f48e0022930b0237e7f3ab8c700fd5
hellosehyun <sehyun030526@gmail.com>
2024-06-19 12:18:42 +0900

    feat: psql config & pool 추가
    psqlConfig 정의와 psqlPool 반환 함수 util 추가

---
+++ src/configs/psqlConfig.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const psqlConfig = {
+  host: process.env.DB_PSQL_HOST,
+  database: process.env.DB_PSQL_DB,
+  user: process.env.DB_PSQL_USER,
+  port: process.env.DB_PSQL_PORT,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+}
+
+export default psqlConfig
