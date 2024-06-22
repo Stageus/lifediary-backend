aa53d32c76870f6e6982626b29aa8450f49441de
Sunny <rladudtjs154@naver.com>
2024-06-22 14:23:01 +0900

    refactor: 파일 구조 재설정

--- src/modules/account/services/accountService.js
+++ src/modules/account/services/accountService.js
@@ -1,15 +1,7 @@
 import axios from "axios";
-<<<<<<< HEAD:src/modules/account/services/accountService.js
 import accountModel from "../../../shared/models/accountModel.js";
 import jwt from "../../../shared/utils/jwt.js";
 import psqlConnect from "../../../shared/utils/psqlConnect.js";
-=======
-import accountModel from "../models/accountModel.js";
-import jwtSign from "../utils/jwt.js";
-import jwtVerify from "../utils/jwtVerify.js";
-import CONSTANTS from "../utils/constansts.js";
-import sendError from "../utils/sendError.js";
->>>>>>> 6511308 (feat: Update nickname 구현):src/services/accountService.js
 
 const accountService = {
   getTokenInfo: (req, res) => {
@@ -50,23 +42,9 @@ const accountService = {
     });
 
     let result = {};
-<<<<<<< HEAD:src/modules/account/services/accountService.js
     const account = await psqlConnect.query(
       accountModel.selectFromGoogleId({ oauthGoogleId: googleAccountInfo.data.id })
     );
-=======
-    const account = await accountModel
-      .selectFromGoogleId({
-        id: googleAccountInfo.data.id,
-      })
-      .catch(() =>
-        sendError({
-          status: 500,
-          message: CONSTANTS.MSG[500],
-          stack: err.stack,
-        })
-      );
->>>>>>> 6511308 (feat: Update nickname 구현):src/services/accountService.js
 
     if (account) {
       const token = jwt.sign({
@@ -90,71 +68,11 @@ const accountService = {
   selectIdx: async (req, res) => {
     const { idx } = jwtVerify(req.headers.token);
 
-<<<<<<< HEAD:src/modules/account/services/accountService.js
     const account = await accountModel.selectFromIdx({ idx: idx });
 
     return account;
   },
   insertAccount: () => {},
-=======
-    if (!idx) {
-      sendError({ status: 404, message: CONSTANTS.MSG[404] });
-    }
-
-    const account = await accountModel.selectMe({ idx: idx }).catch(() =>
-      sendError({
-        status: 500,
-        message: CONSTANTS.MSG[500],
-        stack: err.stack,
-      })
-    );
-
-    return account;
-  },
-
-  insertAccount: async (req, res) => {
-    const { profileImg, nickname, oauthGoogleId } = req.body;
-
-    const account = await accountModel
-      .insert({
-        oauthGoogleId: oauthGoogleId,
-        profileImg: profileImg,
-        nickname: nickname,
-      })
-      .catch(() =>
-        sendError({
-          status: 500,
-          message: CONSTANTS.MSG[500],
-          stack: err.stack,
-        })
-      );
-
-    const token = jwtSign({
-      profileImg: account.profileImg,
-      idx: account.idx,
-      permission: account.permission,
-    });
-
-    return { token: token };
-  },
-  updateNickname: async (req, res) => {
-    const { idx } = jwtVerify(req.headers.token);
-
-    if (!idx) {
-      sendError({ status: 401, message: CONSTANTS.MSG[404] });
-    }
-    const { nickname } = req.body;
-
-    const updateAccount = await accountModel.updateNickname({
-      idx: idx,
-      nickname: nickname,
-    });
-    if (updateAccount.rowCount === 0) {
-      sendError({ status: 409, message: CONSTANTS.MSG[409] });
-    }
-    return;
-  },
->>>>>>> 6511308 (feat: Update nickname 구현):src/services/accountService.js
   selectAccount: () => {},
   update: () => {},
   delete: () => {},
