f95c925ee0ffd64a57885843c2636db9e727bf5d
hellosehyun <sehyun030526@gmail.com>
2024-06-22 18:12:10 +0900

    feat: notice schema 및 validator 적용
    validator 미들웨어를 통한 notice 유효성 검사 schmea 추가

--- src/modules/notice/routes/noticeRoute.js
+++ src/modules/notice/routes/noticeRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import noticeController from "../controllers/noticeController.js";
+import validator from "../../../shared/middlewares/validator.js";
+import noticeSchema from "../schema/noticeSchema.js";
 
 const noticeRoute = express.Router();
 
 noticeRoute
-  .get("/", noticeController.selectNotices)
+  .get("/", validator(noticeSchema.selectNotices), noticeController.selectNotices)
   .get("/new", noticeController.selectIsNew)
-  .delete("/:noticeIdx", noticeController.delete);
+  .delete("/:noticeIdx", validator(noticeSchema.delete), noticeController.delete);
 
 export default noticeRoute;
