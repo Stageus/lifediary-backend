d5e430df2dbc09e046981686846cabd4ca82c50a
hellosehyun <101646633+hellosehyun@users.noreply.github.com>
2024-06-22 22:16:56 +0900

    feat: tagModel & likeModel 추가 (#15)
    * feat: likeModel insertWithUpdate 추가
    
    기존 like 없을시 insert하고 있으면 update
    
    * feat: tagModel insertMany 추가
    
    배열로 태그를 받아 tag table에 각각 insert해주는 함수
    
    * feat: tagModel updateMany 추가 및 deleteMany 삭제
    
    일기 태그 수정시 row들도 업데이트 되고 부족하면 insert, 남으면 delete해주는 함수
    diary가 soft delete인 상황에서 tag의 물리 삭제는 필요 없어 보임

--- src/shared/models/likeModel.js
+++ src/shared/models/likeModel.js
@@ -1,5 +1,20 @@
 const likeModel = {
-  insertUpdateLike: () => {},
-}
+  insertWithUpdate: ({ accountIdx, diaryIdx }) => {
+    return {
+      sql: `
+        WITH updateLike AS (
+          UPDATE "like"
+          SET isDeleted = NOT isDeleted
+          WHERE accountIdx = $1 AND diaryIdx = $2
+          RETURNING 1
+        )
+        INSERT INTO "like" (accountIdx, diaryIdx)
+        SELECT $1, $2
+        WHERE NOT EXISTS (SELECT 1 FROM updateLike)
+      `,
+      values: [accountIdx, diaryIdx],
+    };
+  },
+};
 
-export default likeModel
+export default likeModel;
--- src/shared/models/tagModel.js
+++ src/shared/models/tagModel.js
@@ -1,6 +1,52 @@
 const tagModel = {
-  insertMany: () => {},
-  deleteMany: () => {},
-}
+  insertMany: ({ diaryIdx, tags }) => {
+    return {
+      sql: `
+          INSERT INTO tag (diaryIdx, tagName)
+          SELECT $1, newTags.tagName
+          FROM ( SELECT unnest($2::text[]) AS tagName ) AS newTags;
+        `,
+      values: [diaryIdx, tags],
+    };
+  },
+  updateMany: ({ diaryIdx, tags }) => {
+    return {
+      sql: `
+        WITH newTags AS ( 
+          SELECT unnest($2::text[]) AS tagName,
+          generate_series(1, array_length($2::text[], 1)) AS position
+        ),
+        existingTags AS ( 
+          SELECT idx, tagName, row_number() OVER (ORDER BY idx) AS position
+          FROM tag
+          WHERE diaryIdx = $1
+        ),
+        updateTags AS (
+          UPDATE tag
+          SET tagName = nt.tagName
+          FROM existingTags AS et
+          JOIN newTags AS nt ON et.position = nt.position
+          WHERE tag.idx = et.idx AND tag.diaryIdx = $1
+        ),
+        insertTags AS (
+          INSERT INTO tag (diaryIdx, tagName)
+          SELECT $1, nt.tagName
+          FROM newTags AS nt
+          LEFT JOIN existingTags AS et ON nt.position = et.position
+          WHERE et.tagName IS NULL
+        )
+        DELETE FROM tag
+        WHERE diaryIdx = $1
+        AND idx NOT IN (
+          SELECT idx FROM tag
+          WHERE diaryIdx = $1
+          ORDER BY idx
+          LIMIT array_length($2::text[], 1)
+        );
+    `,
+      values: [diaryIdx, tags],
+    };
+  },
+};
 
-export default tagModel
+export default tagModel;
