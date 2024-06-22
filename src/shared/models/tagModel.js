6af7e0ae0dbc3b82d857f16466a8d36399f0efba
hellosehyun <sehyun030526@gmail.com>
2024-06-22 22:13:49 +0900

    feat: tagModel insertMany 추가
    배열로 태그를 받아 tag table에 각각 insert해주는 함수

--- src/shared/models/tagModel.js
+++ src/shared/models/tagModel.js
@@ -1,6 +1,15 @@
 const tagModel = {
-  insertMany: () => {},
+  insertMany: ({ diaryIdx, tags }) => {
+    return {
+      sql: `
+          INSERT INTO tag (diaryIdx, tagName)
+          SELECT $1, newTags.tagName
+          FROM ( SELECT unnest($2::text[]) AS tagName ) AS newTags;
+        `,
+      values: [diaryIdx, tags],
+    };
+  },
   deleteMany: () => {},
-}
+};
 
-export default tagModel
+export default tagModel;
