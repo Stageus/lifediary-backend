ddadc9f4d9b07a549ed1f9afe6a72cb0d491b3d4
hellosehyun <sehyun030526@gmail.com>
2024-06-26 05:46:31 +0900

    chore: jwt verify 수정
    token 변조되었을경우 error처리 추가

--- src/shared/utils/jwt.js
+++ src/shared/utils/jwt.js
@@ -1,12 +1,20 @@
 import jsonwebtoken from "jsonwebtoken";
 import jwtConfig from "../configs/jwtConfig.js";
+import sendError from "./sendError.js";
+import CONSTANTS from "./constansts.js";
 
 const jwt = {
   sign: (obj) => {
     return jsonwebtoken.sign(obj, process.env.JWT_KEY, jwtConfig);
   },
   verify: (token) => {
-    return !!token && jsonwebtoken.verify(token, process.env.JWT_KEY);
+    try {
+      return !!token && jsonwebtoken.verify(token, process.env.JWT_KEY);
+    } catch (err) {
+      if (err.message === "jwt malformed") {
+        sendError({ message: CONSTANTS.MSG[401], status: 401 });
+      }
+    }
   },
 };
 
