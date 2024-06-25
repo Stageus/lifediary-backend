5dfb3f00e9e8899493f8f50a54c85c40bf3cc7c0
hellosehyun <101646633+hellosehyun@users.noreply.github.com>
2024-06-26 00:33:44 +0900

    feat: grass 라우터 추가  (#16)
    * feat: grass 라우터 추가
    
    해당 년도 또는 오늘로부터 1년동안의 일기 잔디 조회
    
    * chore: diaryModel selectGrass 날짜 관련 수정
    
    new Date()에 사용에 따른 시차 발생 문제 수정
    
    * feat: grass schema 추가 및 자잘한 수정
    
    grass 유효성 처리용 schema 추가 및 미들웨어 연결
    grass Model 날짜 부분 수정
    
    * chore: diaryModel 일부 수정
    
    2023-01-01 ~ 2024-01-01 에서 2023-01-01 ~ 2023-12-31과 같이 변경
    
    * chore: diaryModel grass 수정
    
    createdAt 컬럼 제거

--- src/modules/grass/routes/grassRoute.js.js
+++ src/modules/grass/routes/grassRoute.js.js
@@ -1,8 +1,11 @@
 import express from "express";
+import grassController from "../controllers/grassController.js";
+import grassSchema from "../schema/grassSchema.js";
+import validator from "../../../shared/middlewares/validator.js";
 
 const grassRoute = express.Router();
 
-grassRoute; //
-// .get("/grass", grassController);
+grassRoute //
+  .get("/", validator(grassSchema.get), grassController.get);
 
 export default grassRoute;
