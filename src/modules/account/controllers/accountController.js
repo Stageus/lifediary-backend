f9827ddc5821b6c70e21b7f3a36764c7d19f3cfe
Sunny <96819706+DDOONG211107@users.noreply.github.com>
2024-06-25 13:49:01 +0900

    fix: Account fix (#18)
    * chore: Account controller 수정
    
    * chore: logs 폴더 삭제

--- src/modules/account/controllers/accountController.js
+++ src/modules/account/controllers/accountController.js
@@ -81,42 +81,6 @@ const accountController = {
       next(err);
     }
   },
-
-  get: async (req, res) => {
-    try {
-      const result = await accountService.selectIdx(req, res);
-      res.status(200).send(result);
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  signup: async (req, res) => {
-    try {
-      const result = await accountService.insertAccount(req, res);
-      res.status(200).send(result);
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  updateNickname: async (req, res) => {
-    try {
-      await accountService.updateNickname(req, res);
-      res.sendStatus(200);
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  getIsNicknameExist: async (req, res) => {
-    try {
-      await accountService.selectNickname(req, res);
-      res.sendStatus(200);
-    } catch (err) {
-      next(err);
-    }
-  },
 };
 
 export default accountController;
